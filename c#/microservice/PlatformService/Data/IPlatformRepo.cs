6f372e5d524a92cfa4783e1425f68ec11953c846
joao calado <jcaladomoura@gmail.com>
2022-10-07 22:49:02 -0300

    C# microservice start

---
+++ c#/microservice/PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,10 @@
+using PlatformService.Models;
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+    }
+}
